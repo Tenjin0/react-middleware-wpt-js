ccff6d01ac470833c614ad2a8a9e352d86b4a4b8
Patrice <ppetit@wynd.eu>
2019-09-09 11:44:51 +0200

    feature/js: js version [WIP]

---
+++ src/app/App.jsx
@@ -0,0 +1,19 @@
+import React , {Component} from 'react';
+
+export default class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+    
+    componentDidMount() {
+       
+    }
+
+  render() {
+    return (
+     <div>
+         Hello
+     </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV ? process.env.NODE_ENV === "development" : true
+const config = {
+  mode: devMode ?"development" : "production",
+  entry: {
+    app: [path.join(__dirname, './src/main.jsx')],
+    vendor: [
+      'react', 'react-dom', 'redux', 'react-redux', 'react-bootstrap', 'reactstrap', 'socket.io-client', 'redux-thunk', '@fortawesome/fontawesome-svg-core'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/",
+    filename: 'js/[name].bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'] // .mjs must be before .js
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('autoprefixer')
+              ],
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      baseUrl: "/"
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+if (devMode) {
+  config.entry.app.push('webpack-hot-middleware/client')
+}
+
+module.exports = config
